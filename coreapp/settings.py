d356daea6d1db42f705adc05e9449d9b408281da
Anish <shresthaanishvar@gmail.com>
2025-12-25 20:51:07 +0545

    handle settings

--- coreapp/settings.py
+++ coreapp/settings.py
@@ -76,14 +76,16 @@ WSGI_APPLICATION = 'coreapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'postgres',
+        "USER": 'postgres',
+        "PASSWORD": 'trxphysiotherapy@1',
+        "HOST": 'db.nthifwbnjeexluvjhrmw.supabase.co',
+        "PORT": '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
