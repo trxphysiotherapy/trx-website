bbdb1b2c1b3f0297d57a59220070cdf0642b8432
trxphysiotherapy <trxphysiotherapy@gmail.com>
2025-12-26 19:27:55 +0545

    Update settings.py

--- coreapp/settings.py
+++ coreapp/settings.py
@@ -137,13 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
