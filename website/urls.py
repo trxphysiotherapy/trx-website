61227dc497f6bb96b759c62598b21ff91ef41c6b
Anish <shresthaanishvar@gmail.com>
2026-01-01 21:53:50 +0545

    added testimonial, connect all navbars and local storaging dark/light theme

--- website/urls.py
+++ website/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import home, about, contact, blog, services
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name='home'),
+    path("", home, name="home"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
+    path("blog/", blog, name="blog"),
+    path("services/", services, name="services"),
 ]
 
 # Serve media files in development
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
