a7823b5dc6b1beff23a61bdd6663b836adfa1496
Anish <shresthaanishvar@gmail.com>
2026-01-02 21:06:23 +0545

    made condition we treat handled from django dashboard

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import NavBarItem, Location, Testimonial
+from .models import NavBarItem, Location, Testimonial, Condition
 
 
 admin.site.register(NavBarItem)
 admin.site.register(Location)
 admin.site.register(Testimonial)
+admin.site.register(Condition)
